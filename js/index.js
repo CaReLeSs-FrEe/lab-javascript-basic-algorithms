dad44e0010ee36aa5c69318686846ae038f4e0fa
Alejandro Mejia <mejia.alejandro1@outlook.com>
2022-08-17 13:31:38 -0400

    Iteration 1 & 2 Done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,37 @@
 // Iteration 1: Names and Input
-let hacker1 = "Bob"
-console.log(`The driver's name is ${hacker1}`)
-let hacker2 = "Joe"
-console.log(`The navigator's name is ${hacker2}`)
+
+let hacker1 = `Billy`;
+let hacker2 = `Bob`;
+
+const driverName = hacker1;
+const navigatorName  = hacker2;
+
+
+console.log(`The driver's name is ${driverName}.`);
+
+console.log(`The navigator's name is ${navigatorName}.`);
+
 // Iteration 2: Conditionals
+
+
+if(hacker1 == driverName) {
+    console.log(`Driver` + ' ' + hacker1 + ' ' + `Ready!`);
+} else {
+        console.log(`Can't go anywhere without a driver!.`);
+}
+
+if(hacker2 == navigatorName) {
+    console.log(`Navigator` + ' ' + hacker2 + ' ' + `Let's Go!`);
+ } else {
+        console.log(`Can't navigate without a navigator!`);
+}
+
+if('hacker1' && 'hacker2') {
+    console.log(hacker1 + ' ' + hacker2 + ' ' + 'READY!');
+} else {
+    console.log(`No Crew! Not Ready!`);
+}
+    
 
 
 // Iteration 3: Loops
